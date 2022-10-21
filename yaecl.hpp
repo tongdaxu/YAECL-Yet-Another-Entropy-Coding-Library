5bf24eaac7cea23d868aae3e5601e42516036fbf
许通达 <xutd@air-server>
2022-10-21 13:41:23 +0800

    code dev: align ac according to the paper

--- yaecl.hpp
+++ yaecl.hpp
@@ -85,34 +85,30 @@ template <typename T_in, typename T_out>
  * * default: uint32_t for cxx, int32 for python
  */
 class ArithmeticCodingEncoder {
+  /* according to paper: ARITHMETIC CODING FOR DATA COMPRESSION
+   */
   public:
     BitStream bit_stream;
     ArithmeticCodingEncoder(const int &precision){
         /* precision: 
-         * * internal precision of arithmetic coding, always >= 2 but smaller than T_in
-         * * should be at least 2 bits larger than bits allowed for cdf precision
-         * * and cdf_bits + precision should be smaller than T_in
-         * * this means that with internal width 64, the best cdf precision you can have is
-         * * 31, which means that precision bits should be 33
+         * * aka Code_value_bits
+         * * internal precision of arithmetic coding
+         * * following paper ARITHMETIC CODING FOR DATA COMPRESSION
+         * * requires:
+         * * f \le c - 2 && f + c \le p
          * * default: 32
          */
-        assert(precision >= 2 && precision <= std::numeric_limits<decltype(_full_range)>::digits);
+        assert(precision >= 2 && precision < std::numeric_limits<decltype(_full_range)>::digits);
         _precision = precision;
-        _frequency_bits = std::numeric_limits<decltype(_full_range)>::digits - _precision;
-        _frequency_bits = std::min(_frequency_bits, _precision - 2);
-    	_full_range = static_cast<decltype(_full_range)>(1) << _precision;
-        _half_range = _full_range >> 1;
-        _quarter_range = _half_range >> 1;
-        _three_forth_range = 3 * _quarter_range;
-        _min_range = _quarter_range + 2;
-        _max_total = (static_cast<decltype(_full_range)>(1) << _frequency_bits) - 1;
-        /* bits for max cdf: 
-         *    should not greater than precision bits - 2
-         *    should not greater than internal representation bits - precision bits */
-        _mask = _full_range - 1;
+    	_full_range = (static_cast<decltype(_full_range)>(1) << _precision) - 1;
+        _quarter_range = (_full_range >> 2) + 1;
+        _half_range = _quarter_range * 2;
+        _three_forth_range = _quarter_range * 3;
+        int max_total_bits = std::min(_precision - 2, std::numeric_limits<decltype(_full_range)>::digits - _precision);
+        _max_total = (static_cast<decltype(_full_range)>(1) << max_total_bits) - 1;
         _low = 0;
-        _high = _mask;
-        _pending_bits=0;
+        _high = _full_range;
+        _pending_bits = 0;
     }
     ~ArithmeticCodingEncoder(){}
     void encode(const T_out &sym, const T_out *cdf, const int &cdf_bits){
@@ -125,11 +121,9 @@ class ArithmeticCodingEncoder {
          * * 2 ** cdf_bits == last element of cdf, always <= _frequency_bits
          */
         assert(_low < _high);
-        assert((_low & _mask) == _low);
-        assert((_high & _mask) == _high);
+        assert((_low & _full_range) == _low);
+        assert((_high & _full_range) == _high);
         T_in range = _high - _low + 1;
-        assert(_min_range <= range);
-        assert(range <= _full_range);
         T_in c_total = static_cast<decltype(c_total)>(1) << cdf_bits;
         assert(c_total <= _max_total);
         T_in c_low = cdf[sym];
@@ -137,12 +131,29 @@ class ArithmeticCodingEncoder {
         assert(c_low != c_high);
         _high = _low + ((c_high * range) >> cdf_bits) - 1;
         _low  = _low + ((c_low  * range) >> cdf_bits);
+        _renormalize();
+    }
+    void flush(){
+        /* call before the end of encoding */
+        _pending_bits++;
+        bool bit = static_cast<bool>(_low >= _quarter_range);
+        bit_stream.push_back(bit);        
+        for (; _pending_bits > 0; _pending_bits--)
+            bit_stream.push_back(!bit);
+    }
+  private:
+    void _renormalize(){
         while(1){
-            if(_high<_half_range||_low>=_half_range){
-                bool bit = static_cast<bool>(_high>=_half_range);
-                bit_stream.push_back(bit);        
+            if(_high < _half_range){
+                bit_stream.push_back(0);        
                 for (; _pending_bits > 0; _pending_bits--)
-                    bit_stream.push_back(!bit);
+                    bit_stream.push_back(1);
+            } else if (_low >= _half_range){
+                bit_stream.push_back(1);        
+                for (; _pending_bits > 0; _pending_bits--)
+                    bit_stream.push_back(0);
+                _low -= _half_range;
+                _high -= _half_range;
             }else if(_low>=_quarter_range&&_high<_three_forth_range){
                 assert(_pending_bits < std::numeric_limits<decltype(_pending_bits)>::max());
                 _pending_bits++;
@@ -151,32 +162,16 @@ class ArithmeticCodingEncoder {
             }else{
                 break;
             }
-            _high <<= 1;
-            _high++;
+            _high = (_high << 1) + 1;
             _low <<=1;
-            _high &= _mask;
-            _low &= _mask;
         }
     }
-    void flush(){
-        /* call before the end of encoding
-         */
-        _pending_bits++;
-        bool bit = static_cast<bool>(_low >= _quarter_range);
-        bit_stream.push_back(bit);        
-        for (; _pending_bits > 0; _pending_bits--)
-            bit_stream.push_back(!bit);
-    }
-  private:
     T_in _precision;
-    T_in _frequency_bits;
     T_in _full_range;
     T_in _half_range;
     T_in _quarter_range;
     T_in _three_forth_range;
-    T_in _min_range;
     T_in _max_total;
-    T_in _mask;
     T_in _low;
     T_in _high;
     T_in _pending_bits;
@@ -192,25 +187,22 @@ class ArithmeticCodingDecoder{
          * encode_bit_stream:
          * * the BitStream ro decode from encoder / read from file
          */
-        assert(precision >= 2 && precision <= std::numeric_limits<decltype(_full_range)>::digits);
         bit_stream = encode_bit_stream;
+        assert(precision >= 2 && precision < std::numeric_limits<decltype(_full_range)>::digits);
         _precision = precision;
-        _frequency_bits = std::numeric_limits<decltype(_full_range)>::digits - _precision;
-        _frequency_bits = std::min(_frequency_bits, _precision - 2);
-    	_full_range = static_cast<decltype(_full_range)>(1) << _precision;
-        _half_range = _full_range >> 1;
-        _quarter_range = _half_range >> 1;
-        _three_forth_range = 3 * _quarter_range;
-        _min_range = _quarter_range + 2;
-        _max_total = (static_cast<decltype(_full_range)>(1) << _frequency_bits) - 1;
-        _mask = _full_range - 1;
+    	_full_range = (static_cast<decltype(_full_range)>(1) << _precision) - 1;
+        _quarter_range = (_full_range >> 2) + 1;
+        _half_range = _quarter_range * 2;
+        _three_forth_range = _quarter_range * 3;
+        _max_total = (static_cast<decltype(_full_range)>(1) << (_precision - 2)) - 1;
+        _max_total = std::min(_max_total, (static_cast<decltype(_full_range)>(1) << (std::numeric_limits<decltype(_full_range)>::digits - _precision - 1)) - 1);
         _low = 0;
-        _high = _mask;
+        _high = _full_range;
         _pending_bits = 0;
         _code = 0;
         for(int i=0;i<_precision;i++){
             _code <<= 1;
-            _code += bit_stream.pop_front() ? 1: 0;
+            _code += static_cast<int>(bit_stream.pop_front());
         }
     }
     ~ArithmeticCodingDecoder(){}
@@ -224,6 +216,7 @@ class ArithmeticCodingDecoder{
          * * 2 ** cdf_bits == last element of cdf, always <= _frequency_bits
          */
         T_in c_total = static_cast<decltype(c_total)>(1) << cdf_bits;
+        assert(c_total <= _max_total);
         T_in range = _high - _low + 1;
         T_in scaled_range = _code - _low;
         T_in scaled_value = (((scaled_range + 1) << cdf_bits) - 1) / range;
@@ -242,9 +235,13 @@ class ArithmeticCodingDecoder{
         T_in c_low = cdf[sym];
         T_in c_high = cdf[sym + 1];
         assert(c_low != c_high);
-        assert(c_total <= _max_total);
         _high = _low + ((c_high * range) >> cdf_bits) - 1;
         _low  = _low + ((c_low  * range) >> cdf_bits);
+        _renormalize();
+        return static_cast<T_out>(sym);
+    }
+  private:
+    void _renormalize(){
         while(1){
             if(_high < _half_range){
                 // pass
@@ -259,28 +256,63 @@ class ArithmeticCodingDecoder{
             }else{
                 break;
             }
-            _high <<= 1;
-            _high++;
+            _high = (_high << 1) + 1;
             _low <<=1;
-            _code <<= 1;
-            _code += bit_stream.pop_front() ? 1 : 0;
+            _code = (_code << 1) + static_cast<int>(bit_stream.pop_front());
         }
-        return static_cast<T_out>(sym);
     }
-  private:
     T_in _precision;
-    T_in _frequency_bits;
     T_in _full_range;
     T_in _half_range;
     T_in _quarter_range;
     T_in _three_forth_range;
-    T_in _min_range;
     T_in _max_total;
-    T_in _mask;
     T_in _low;
     T_in _high;
     T_in _pending_bits;
     T_in _code;
+};
+
+class RANGEEncoder {
+  public:
+  BitStream bit_stream;
+    RANGEEncoder(){
+        _h_precision = 26;
+        _t_precision = 8; // 1 byte
+        _o_precision = _h_precision - _t_precision;
+        _low = 0;
+        _range = (1 << _h_precision); // 2 ** 32
+    }
+    ~RANGEEncoder(){}
+    void encode(const int &sym, const uint32_t *cdf, const int &cdf_bits){
+        uint64_t c_low = cdf[sym];
+        uint64_t c_high = cdf[sym + 1];
+        uint64_t c_total = static_cast<decltype(c_total)>(1) << cdf_bits;
+        uint64_t c_range = c_high - c_low;
+        _range >>= cdf_bits;
+        printf("%lld %lld\n", static_cast<long long>(_low), static_cast<long long>(_range));
+        _low = _low + c_low * _range;
+        printf("%lld %lld\n", static_cast<long long>(_low), static_cast<long long>(_range));
+        _range *= c_range;
+        printf("%lld %lld\n", static_cast<long long>(_low), static_cast<long long>(_range));
+        assert(0);
+        while ((_low >> _o_precision) == ((_low + _range) >> _o_precision)){
+            bit_stream.push_back_byte(static_cast<uint8_t>(_low >> (_o_precision)));
+            // emit one byte
+            _low = (_low % (static_cast<decltype(_low)>(1) << _o_precision)) << _t_precision;
+            _range <<= _t_precision;
+            printf("%d %d\n", static_cast<int>(_low), static_cast<int>(_range));
+            if(_range == 0 && _low == 0) assert (0);
+        }
+    }
+  private:
+    uint64_t _h_precision;
+    uint64_t _t_precision;
+    uint64_t _o_precision;
+    uint64_t _low;
+    uint64_t _high;
+    uint64_t _range;
+    uint64_t _pending_bits;
 };
 template <typename T_in, typename T_out>
 /* template args: see ArithmeticCodingEncoder */
@@ -314,10 +346,11 @@ class RANSCodec {
         _t_precision = t_precision;
         _h_min = static_cast<decltype(_h_min)>(1) << (_h_precision - _t_precision);
         bit_stream = encode_bit_stream;
-         _state = 0;
-        for(int i = _h_precision - 1; i >= 0; i--){
-            T_in bit = bit_stream.pop_back();
-            _state |= (bit << i);
+        _state = 0;
+        for(int i = 1; i <= _h_precision / 8; i++){
+            _state <<= 8;
+            uint8_t byte = bit_stream.pop_back_byte();
+            _state |= byte;
         }
     }
     ~RANSCodec(){}
@@ -329,30 +362,21 @@ class RANSCodec {
         T_in state = _state;
         T_in state_max = c_range << (_h_precision - cdf_bits);
         if(state >= state_max){
-            T_in mk=0xff;
+            T_in mask = 0xff;
             for(int i = 1; i <= _t_precision / 8; i++){
-                bit_stream.push_back_byte(static_cast<uint8_t>(state & mk));
+                bit_stream.push_back_byte(static_cast<uint8_t>(state & mask));
                 state >>= 8;
             }
-            /*
-            T_in mask = 1;
-            for(int i = 1; i <= _t_precision; i++){
-                bit_stream.push_back(static_cast<bool>(state & mask));
-                mask <<= 1;
-            }
-            state >>= _t_precision;
-            */
             assert(state < state_max);
         }
         _state = ((state / c_range) << cdf_bits) + (state % c_range) + c_low;
     }
     void flush(){
-        T_in mask = 1;
-        for(int i = 1; i <= _h_precision; i++){
-            bit_stream.push_back(static_cast<bool>(_state & mask));
-            mask <<= 1;
+        T_in mask = 0xff;
+        for(int i = 1; i <= _h_precision / 8; i++){
+            bit_stream.push_back_byte(static_cast<uint8_t>(_state & mask));
+            _state >>= 8;
         }
-        _state = 0;
     }
     T_out decode(const int &sym_cnt, const T_out *cdf, const int &cdf_bits){
         /* args: See ArithmeticCodingDecoder */
@@ -378,13 +402,6 @@ class RANSCodec {
                 uint8_t byte = bit_stream.pop_back_byte();
                 state |= byte;
             }
-            /*
-            state <<= _t_precision;
-            for(int i = _t_precision - 1; i >= 0; i--){
-                T_in bit = bit_stream.pop_back();
-                state |= (bit << i);
-            }
-            */
             assert (state >= _h_min);
         }
         _state = state;
